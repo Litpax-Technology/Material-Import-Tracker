7e9c794ee7d4786b77d949414abbdd22e2ed3dc0
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-02 22:08:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Litpax Material Import Tracker — Config
 // GAS Web App URL yahan daalo (deploy ke baad)
 // ============================================
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwA6JxOhUg92VJvoBktR1Tqygdfe8hh9yV24OJtPv7eUb8ShZdG_jlzGaVLxZGmJtrqRA/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzSyocKvpi9fA0RIdrXTe4BWvTuiwax6HwupqlcnMSK2fG5wnGpTmmnl0NehkIA6CVUdw/exec';

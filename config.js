78a1516c9565c9beecf0ac48edf945231ea99911
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-03 09:34:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ============================================
 // Litpax Material Import Tracker — Config
 // ============================================
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbw2kI94ZXBT7hWg_mrHONRD-LjiskHADyuphWn0Tt3GgPhanjDsaAihRKEg_EIdU54cBA/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwzkMo0hXWwW8wo5ePIsd3ybbk6Jieu3P4mjc5IXox-Kf9SjGTN69qv2kGXHJABqsDZzQ/exec';
